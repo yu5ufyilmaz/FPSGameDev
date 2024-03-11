4c6b7766e5fdc4095a97a8bf5ad1dbca6717d9f6
Yusuf Yilmaz <95490228+yu5ufyilmaz@users.noreply.github.com>
2024-03-11 14:09:11 +0300

    Input Manager is Done

--- Assets/Scripts/Player/PlayerInteract.cs
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -10,15 +10,12 @@ public class PlayerInteract : MonoBehaviour
     [SerializeField] private float distance = 3f;
     [SerializeField] private LayerMask mask;
     private PlayerUI _playerUI;
-    private PlayerInput _playerInput;
-
-    private void Awake()
-    {
-        _playerInput = new PlayerInput();
-    }
+    private InputManager _inputManager;
+    
 
     private void Start()
     {
+        _inputManager = GetComponent<InputManager>();
         cam = GetComponent<PlayerLook>().cam;
         _playerUI = GetComponent<PlayerUI>();
     }
@@ -36,7 +33,7 @@ public class PlayerInteract : MonoBehaviour
                 Debug.Log("Tepkiver");
                 Interactable interactable = hitInfo.collider.GetComponent<Interactable>();
                 _playerUI.UpdateText(interactable.promptMessage);
-                if (_playerInput.OnFoot.Interact.triggered)
+                if (_inputManager._onFootActions.Interact.triggered)
                 {
                     Debug.Log("Interacted");
                     interactable.BaseInteract();
