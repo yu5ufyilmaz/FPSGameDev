ed432fde710c07be5665b311340d4c9433876394
Yusuf Yilmaz <95490228+yu5ufyilmaz@users.noreply.github.com>
2024-03-18 00:57:59 +0300

    Character Health System and URP
    I make the ui system for character damage and now its showing with the ui.
    Also I change graphich from Built in to URP

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
+    public bool useEvents;
     public string promptMessage;
 
     public void BaseInteract() 
     {
+        if (useEvents)
+        {
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
+        }
         Interact();
     }
 
---
+++ Assets/Scripts/InteractionEvent.cs
@@ -0,0 +1,18 @@
+using UnityEngine.Events;
+using UnityEngine;
+
+public class InteractionEvent : MonoBehaviour
+{
+    public UnityEvent OnInteract;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
