71a8b707d283205b855d212305f79c59978384cf
Yusuf Yilmaz <95490228+yu5ufyilmaz@users.noreply.github.com>
2024-03-20 18:16:25 +0300

    WeaponSystemDebugging

---
+++ Assets/Weapon.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 30f;
+    public float bulletPrefabLifeTime = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon()
+    {
+       GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+       bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity,ForceMode.Impulse);
+       StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+}
